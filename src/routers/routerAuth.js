1abae81f0713d941a4db6cecc01719bc8f616de2
João Veloso <141861100+joaoVelosoT@users.noreply.github.com>
2025-02-05 17:31:16 -0300

    feat: adicionando o loginUser, falta apenas testar

--- src/routers/routerAuth.js
+++ src/routers/routerAuth.js
@@ -1,9 +1,13 @@
 const { Router } = require("express");
 const RegisterUserController = require("../controllers/AuthControllers/registerUserController");
 const UserCreateValidator = require("../middlewares/Validators/UserValidators/UserCreateValidator");
+const LoginUserController = require("../controllers/AuthControllers/loginUserController");
 const router = Router();
 
 // User Register
 router.post("/register", UserCreateValidator, RegisterUserController);
+
+// User Login
+router.post("/login", LoginUserController);
 
 module.exports = router;
---
+++ src/services/AuthServices/loginUserService.js
@@ -0,0 +1,75 @@
+const User = require("../../models/User");
+const bycrpt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const LoginUserService = async (dataLogin) => {
+  try {
+    // email
+    // password
+
+    const user = await User.findOne({ where: { email: dataLogin.email } });
+    if (!user) {
+      return {
+        code: 400,
+        error: {
+          details: [
+            {
+              field: "email",
+              message: "O email enviado não existe",
+            },
+          ],
+        },
+        message: "Erro ao validar login",
+        success: false,
+      };
+    }
+
+    // se achou o user, validar se a senha e a mesma enviada
+    const passwordDecode = await bycrpt.compareSync(
+      dataLogin.password,
+      user.password
+    );
+
+    if (!passwordDecode) {
+      return {
+        code: 401,
+        error: {
+          details: [
+            {
+              field: "password",
+              message: "Senha incorreta",
+            },
+          ],
+        },
+        message: "Erro ao validar login",
+        success: false,
+      };
+    }
+
+    const token = await jwt.sign(
+      {
+        id: user.user.id,
+        email: user.user.email,
+        isDisabled: user.user.isDisabled,
+        techAss: user.user.techAss,
+      },
+      process.env.SECRET,
+      { expiresIn: "10h" }
+    );
+
+    return {
+      code: 201,
+      data: {
+        token,
+        user: user.user,
+      },
+      message: "login realizado com sucesso",
+      success: true,
+    };
+  } catch (error) {
+    console.error(error);
+    throw new Error(error.message);
+  }
+};
+
+module.exports = LoginUserService;
