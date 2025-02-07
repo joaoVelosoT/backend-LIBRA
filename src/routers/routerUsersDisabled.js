ff870825cedc86333ba78eccde0effb313946715
Raphael Cardoso <raphaelbcjr@gmail.com>
2025-02-07 16:36:12 -0300

    feat: add new function getOneById userDisabled

--- src/routers/routerUsersDisabled.js
+++ src/routers/routerUsersDisabled.js
@@ -1,19 +1,21 @@
 const { Router } = require("express");
 const UsersDisabledCreateController = require("../controllers/UsersDisabledsController/UsersDisabledCreateController");
 const UsersDisabledCreateValidator = require("../middlewares/Validators/UsersDisabledValidators/UsersDisabledCreateValidator");
+const userDisabledGetAllController = require("../controllers/UsersDisabledsController/UsersDisabledGetAllController");
+const userDisabledGetOneController = require("../controllers/UsersDisabledsController/UsersDisabledGetOneController")
+const UserDisabledDeleteController = require("../controllers/UsersDisabledsController/UsersDisabledDeleteController");
 const router = Router();
 
-const userDisabledGetAllController = require('../controllers/UsersDisabledsController/UsersDisabledGetAllController');
 
-const UserDisabledDeleteController = require("../controllers/UsersDisabledsController/UsersDisabledDeleteController");
+
 
 // Create userDisabled
 router.post("/", UsersDisabledCreateValidator, UsersDisabledCreateController);
 
-// GetAll userDisabled
-router.get("/");
-
 // GetOne userDisabled
+router.get("/:id", userDisabledGetOneController.getOneById);
+
+// GetAll userDisabled
 router.get("/", userDisabledGetAllController.getAll);
 
 // Update userDisabled
