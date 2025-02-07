5f8120f2198fbb19d3ad9a2ec239bc81fd8aac2e
Raphael de Barros Cardoso Jr <154370014+RaphaCardoso@users.noreply.github.com>
2025-02-07 00:09:45 -0300

    fix: update router

--- src/routers/router.js
+++ src/routers/router.js
@@ -4,11 +4,13 @@ const router = Router();
 const routerUser = require("./routerUser");
 const routerDisableds = require("./routerDisabled");
 const routerAssistiveTech = require("./routerAssistiveTech");
+const routerTypesDisableds = require("./routerTypeDisabled");
 const routerAuth = require("./routerAuth");
 
-router.use("/users", routerUser );
+router.use("/users", routerUser);
 router.use("/disableds", routerDisableds);
 router.use("/assistivetech", routerAssistiveTech);
+router.use("/typesdisabled", routerTypesDisableds);
 router.use("/auth", routerAuth);
 
 module.exports = router;
