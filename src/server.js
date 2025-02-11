e0be326a8cc0e212f010565787b83a38ae40d347
João Veloso <141861100+joaoVelosoT@users.noreply.github.com>
2025-02-11 10:51:08 -0300

    feat: add cors

--- src/server.js
+++ src/server.js
@@ -4,7 +4,13 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const router = require("./routers/router");
 const valENV = require("./utils/valENV");
+const swaggerUI = require("swagger-ui-express");
+const swaggerDocs = require("../swagger.json");
+const cors = require("cors");
+
 app.use(express.json());
+app.use(cors());
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use(router);
 
 // Validar .env
