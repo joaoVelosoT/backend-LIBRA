9bb7cff9bcfae6bdbfbaa0cd136a4567bd51f290
nandosimoes <fernandodasilvasimoes10@gmail.com>
2025-03-07 14:52:35 -0300

    feat: getAll NotificationResquestedBooks

--- src/routers/routerNotificarion.js
+++ src/routers/routerNotificarion.js
@@ -2,6 +2,9 @@ const { Router } = require("express");
 const NotificationCreateController = require("../controllers/NotificationControllers/NotificationCreateController");
 const NotificationGetAllController = require("../controllers/NotificationControllers/NotificationGetAllController");
 const NotificationDeleteController = require("../controllers/NotificationControllers/NotificationDeleteController");
+
+const NotificationRequestedBookGetAllController = require("../controllers/NotificationRequestedBookControllers/NotificationRequestedBookGetAllController");
+
 const NotificationCreateValidator = require("../middlewares/Validators/NotificationValidators/NotificationCreateValidator");
 const ValidatorID = require("../middlewares/Validators/ValidatorID")
 
@@ -15,5 +18,9 @@ router.get("/", NotificationGetAllController);
 
 // Deletar notificação
 router.delete("/:id", ValidatorID, NotificationDeleteController);
+
+// Listar todas as notificações de livro
+router.get("/books", NotificationRequestedBookGetAllController);
+
 
 module.exports = router;
