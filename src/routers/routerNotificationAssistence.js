d6da8d13d7a4627a6fd5f81f60aac3847fc8b950
Raphael Cardoso <raphaelbcjr@gmail.com>
2025-03-07 14:40:13 -0300

    feat: feat/notificationAssistence complete

--- src/routers/routerNotificationAssistence.js
+++ src/routers/routerNotificationAssistence.js
@@ -2,18 +2,14 @@ const { Router } = require("express");
 
 const notificationAssCreateController = require("../controllers/NotificationAssistenceControllers/NotificationAssCreateController");
 const notificationAssGetAllController = require("../controllers/NotificationAssistenceControllers/NotificationAssGetAllController");
-const notificationAssGetOneController = require("../controllers/NotificationAssistenceControllers/NotificationAssGetOneController");
-const notificationAssUpdateController = require("../controllers/NotificationAssistenceControllers/NotificationAssUpdateController");
-const notificationAssDeleteController = require("../controllers/NotificationAssistenceControllers/NotificationAssDeleteController");
+
 
 const router = Router();
 
 
 router.post("/", notificationAssCreateController);
 router.get("/", notificationAssGetAllController);
-// router.get("/:id", notificationAssGetOneController);
-// router.put("/:id", notificationAssUpdateController);
-// router.delete("/:id", notificationAssDeleteController);
+
 
 
 
