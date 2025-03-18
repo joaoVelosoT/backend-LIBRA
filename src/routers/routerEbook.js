b3fa2e53ae68d1b77ba6147860e7a1aead63e056
Raphael Cardoso <raphaelbcjr@gmail.com>
2025-03-18 15:40:49 -0300

    feat: add create getAll and delete ebook

--- src/routers/routerEbook.js
+++ src/routers/routerEbook.js
@@ -4,11 +4,13 @@ const upload = multer({ storage: multer.memoryStorage() });
 const router = Router();
 
 const EbookCreateController = require("../controllers/EbooksControllers/EbookCreateController.js");
-// const EbookDeleteController = require("../controllers/EbooksControllers/EbookDeleteController.js");
-// const EbookGetAllController = require("../controllers/EbooksControllers/EbookGetAllController.js");
+const EbookDeleteController = require("../controllers/EbooksControllers/EbookDeleteController.js");
+const EbookGetAllController = require("../controllers/EbooksControllers/EbookGetAllController.js");
 
-router.post("/", EbookCreateController);
-// router.get("/", EbookGetAllController);
-// router.delete("/:id", EbookDeleteController);
+router.post("/", upload.fields([
+    { name: "Ebook", maxCount: 10 },
+]), EbookCreateController);
+router.get("/", EbookGetAllController);
+router.delete("/:id", EbookDeleteController);
 
 module.exports = router
