a3393761168d91110fb8075974e3ad20d7225ae8
Raphael Cardoso <raphaelbcjr@gmail.com>
2025-05-27 16:35:36 -0300

    feat: add azure SQL

--- src/database/config.js
+++ src/database/config.js
@@ -7,7 +7,12 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: "mysql",
+    dialect: "mssql",
+    dialectOptions: {
+      options: {
+        encrypt: true
+      }
+    }
   }
 );
 
