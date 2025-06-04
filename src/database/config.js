2aa1e0e1f8bbdf49bec2654b96760be8b6a0bdd6
Fernando <fernandodasilvasimoes10@gmail.com>
2025-06-04 14:34:35 -0300

    fix: emailService

--- src/database/config.js
+++ src/database/config.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: "mssql",
+    dialect: "mysql",
     dialectOptions: {
       options: {
         encrypt: true
